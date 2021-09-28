4c95a0a559232636ee0fcbe5774274712a2b949d
OlgaIsmailova <49490169+OlgaIsmailova@users.noreply.github.com>
2021-09-28 22:53:08 +0300

    HomeTask2.3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,19 +5,14 @@ public class MyFirstProgram {
     //main
     public static void main(String[] args) {
         System.out.println("Hello world");
-        Point o = new Point();
+        Point o = new Point(-1,-1);
+        Point l = new Point(1,1);
+        System.out.println(o.distance(l));
 
-        o.x = 0;
-        o.y = 0;
 
-        Point l = new Point();
-        l.x = 1;
-        l.y = 1;
 
-        System.out.println(distance(o, l));
+
     }
 
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+
     }
-}
