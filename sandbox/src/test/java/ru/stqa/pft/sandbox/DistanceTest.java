d06d73216856b474d472d84bcbe83a6ac14cfc4c
OlgaIsmailova <49490169+OlgaIsmailova@users.noreply.github.com>
2021-09-30 23:04:02 +0300

    Задания 2 и 3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTest.java
@@ -1,13 +1,14 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 public class DistanceTest{
 
     @Test
     public void testDistance() {
 
-        Point o = new Point(0, 0);
+        Point o = new Point(0.0, 0);
         Point l = new Point(1, 1);
-        assert o.distance(o, l) == 1;
+        Assert.assertEquals(o.distance(o, l), 1.4142135623730951);
     }
 }
