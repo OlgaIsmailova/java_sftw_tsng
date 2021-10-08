4c3c79478b3cf0db808da96909d57378968e9cd3
OlgaIsmailova <49490169+OlgaIsmailova@users.noreply.github.com>
2021-10-08 12:28:44 +0300

    Задание 5

--- addressbook-web-tests1/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests1/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -33,23 +33,7 @@ public class ApplicationManager {
         wd.quit();
     }
 
-    public boolean isElementPresent(By by) {
-      try {
-        wd.findElement(by);
-        return true;
-      } catch (NoSuchElementException e) {
-        return false;
-      }
-    }
 
-    public boolean isAlertPresent() {
-      try {
-        wd.switchTo().alert();
-        return true;
-      } catch (NoAlertPresentException e) {
-        return false;
-      }
-    }
 
     public void goToHomePage() {
       wd.findElement(By.linkText("home")).click();
--- addressbook-web-tests1/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests1/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -4,44 +4,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactHelper {
-    private WebDriver wd;
+public class ContactHelper extends HelperBase {
 
     public ContactHelper(WebDriver wd) {
-        this.wd=wd;
+        super(wd);
     }
 
     public void submitContactCreation() {
-      wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+        click(By.xpath("//div[@id='content']/form/input[21]"));
     }
+
 
     public void fillContactPage(ContactData contactData) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-      wd.findElement(By.name("address")).click();
-      wd.findElement(By.name("address")).clear();
-      wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-      wd.findElement(By.xpath("//div[@id='content']/form/label[9]")).click();
-      wd.findElement(By.name("home")).click();
-      wd.findElement(By.name("home")).clear();
-      wd.findElement(By.name("home")).sendKeys(contactData.getPhone());
-      wd.findElement(By.name("email")).click();
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+        type(By.name("firstname"), contactData.getFirstName());
+        type(By.name("lastname"), contactData.getLastName());
+        type(By.name("address"), contactData.getAddress());
+        click(By.xpath("//div[@id='content']/form/label[9]"));
+        type(By.name("home"), contactData.getPhone());
+        type(By.name("email"), contactData.getEmail());
     }
 
+
+
     public void newContactLink() {
-      wd.findElement(By.linkText("add new")).click();
+        click(By.linkText("add new"));
     }
 
     public void deleteSelectedContact() {
-      wd.findElement(By.xpath("//input[@value='Delete']")).click();
+        click(By.xpath("//input[@value='Delete']"));
     }
 
     public void selectContact() {
-      wd.findElement(By.id("8")).click();
+        click(By.id("11"));
     }
 }
